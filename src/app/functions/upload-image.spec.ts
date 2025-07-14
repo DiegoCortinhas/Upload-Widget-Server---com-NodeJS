b3a4929d340188e6f1cbb7446c45dc620c03f3f4
DiegoCortinhas <diego.cortinhas@gmail.com>
2025-07-14 13:35:01 -0300

    testing upload images

---
+++ src/app/functions/upload-image.spec.ts
@@ -0,0 +1,52 @@
+import { Readable } from 'node:stream'
+import {beforeAll, describe,expect,it, vi} from 'vitest'
+import { uploadImage } from './upload-image'
+import { isLeft, isRight, unwrapEither } from '@/shared/either'
+import { uploadFileToStorage } from '@/infra/storage/upload-file-to-storage'
+import { randomUUID } from 'node:crypto'
+import { schema } from '@/infra/db/schemas'
+import { file } from 'zod/v4-mini'
+import { db } from '@/infra/db'
+import { equal } from 'node:assert'
+import { eq } from 'drizzle-orm'
+import { InvalidFileFormat } from './errors/invalid-file-format'
+
+describe('upload image', () => {
+    beforeAll (()=>{
+        vi.mock('@/infra/storage/upload-file-to-storage',()=>{
+            return {
+                uploadFileToStorage: vi.fn().mockImplementation(()=>{
+                    return{
+                        key:`${randomUUID()}.jpg`,
+                        url:'https://storage.com/image.jpg',
+                    }
+                }),
+            }
+        })
+    })
+    it('should be able to upload an image', async() => {
+        const fileName = `${randomUUID()}.jpg`
+        //sut = system under test
+        const sut = await uploadImage({
+            fileName,
+            contentType: 'image/jpg',
+            contentStream: Readable.from([])
+        })
+        expect(isRight(sut)).toBe(true)
+        const result = await db.select().from(schema.uploads).where(eq(schema.uploads.name,fileName))
+        expect(result).toHaveLength(1)
+
+    })
+    it('should not be able to upload an invalid file', async() => {
+        const fileName = `${randomUUID()}.pdf`
+        //sut = system under test
+        const sut = await uploadImage({
+            fileName,
+            contentType: 'document/pdf',
+            contentStream: Readable.from([])
+        })
+        expect(isLeft(sut)).toBe(true)
+        expect(unwrapEither(sut)).toBeInstanceOf(InvalidFileFormat)
+
+    })  
+})
